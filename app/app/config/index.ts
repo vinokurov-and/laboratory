830037f3f8100dc4da05b95da34c4cdd35fec720
Kayoshi40 <savochkin2002rus@yandex.ru>
2023-10-24 12:57:36 +0300

    Добавил вывод заданий

--- app/app/config/index.ts
+++ app/app/config/index.ts
@@ -11,17 +11,15 @@ const LABELS = {
     'webpack-react': 'Администрирование и разработка Web-приложений (Webpack)'
 }
 
-type DisciplinesType = Record<string, {label: string, labs: {
-    file: string, url: string
-}}>[];
+type DisciplinesType = Record<string, {label: string, curPath: string , labs: { file: string, url: string}}>[];
 
 export const DISCIPLINES = disciplinesFolders.reduce((prevValue, curVal, index) => {
 
     const lab = getFolders(resolve(__dirname, PATH+curVal)).reduce((prevLab, curLab, indexLab) => {
-        return {...prevLab, [indexLab+1]: { file: curLab, url: resolve(__dirname, PATH+curVal+curLab)} }
+        return {...prevLab, [indexLab+1]: { file: curLab, url: resolve(__dirname, PATH+curVal+"\\"+curLab)} }
     }, {});
 
     console.log(lab)
 
-
     return {...prevValue, [curVal]: {label: LABELS[curVal], labs: lab}} }, {}) as DisciplinesType;
+
--- app/app/page.tsx
+++ app/app/page.tsx
@@ -3,12 +3,19 @@ import { DISCIPLINES } from './config/index';
 const folders = DISCIPLINES;
 
 export default function Home() {
-  // console.log(MD);
-  return (
-    <main>
-      {Object.keys(folders).map(item => {
-        return <p>{folders[item].label}</p>
-      })}
-    </main>
-  )
+    // console.log(MD);
+    return (
+        <main>
+            {Object.keys(folders).map(item => {
+                return (
+                <div>
+                    <h1>{folders[item].label}</h1>
+                    <div>{Object.keys(folders[item].labs).map(lab =>{
+                        return <h2><a href={folders[item].labs[lab].url}>{folders[item].labs[lab].file}</a></h2>
+                    })}</div>
+                </div>
+                )
+            })}
+        </main>
+    )
 }
